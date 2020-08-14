ed897f92a053eb99a9901eeec5bcf9e113a6dd9d
Claire P <68925298+aws-painec@users.noreply.github.com>
2020-08-14 14:05:37 -0700

    fix(integ): fix for docdb test script (#62)

--- integ/components/deadline/repository/scripts/bastion/testing/DL-read-docdb-response.sh
+++ integ/components/deadline/repository/scripts/bastion/testing/DL-read-docdb-response.sh
@@ -14,6 +14,7 @@ set -euo pipefail
 
 AWS_REGION=$1
 DB_SECRET_ARN=$2
+CERT="$(pwd)/cert"
 
 # Retrieve login information for the database from its Secret
 DB_SECRET_VALUE=$(aws secretsmanager get-secret-value --secret-id=$DB_SECRET_ARN --region=$AWS_REGION)
@@ -21,9 +22,9 @@ DB_SECRET_STRING=$(jq -r '.SecretString' <<< "$DB_SECRET_VALUE")
 DB_USERNAME=$(jq -r '.username' <<< "$DB_SECRET_STRING")
 DB_PASS=$(jq -r '.password' <<< "$DB_SECRET_STRING")
 
-if [ $(ls cert) ]; then
+if [ -d "$CERT" ]; then
   # MongoDB instances require retrieving the value of the .pem key created for the database
-  CERT_CA="$(pwd)/ca-cert.crt"
+  CERT_CA="$CERT/ca-cert.crt"
 
   # The domain zone for the mongo instance used is hard-coded during setup
   DB_ADDRESS="mongo.renderfarm.local:27017"
@@ -40,6 +41,4 @@ fi
 # Mongo command to query for "deadline10db" database
 mongo --quiet --ssl --host="$DB_ADDRESS" --sslCAFile="$CERT_CA" --username="$DB_USERNAME" --password="$DB_PASS" --eval='printjson( db.adminCommand( { listDatabases: 1, nameOnly: true, filter: { "name": "deadline10db" } } ) )'
 
-# Cleanup
-rm -rf "${TMPDIR}"
 exit 0
