1c141c2e1d45f9cf11e428f3c097fc79220089d0
David Horsman <horsmand@amazon.com>
2021-04-08 14:33:02 +0000

    Updated RFDK version in example

--- examples/deadline/Local-Zone/ts/bin/config.ts
+++ examples/deadline/Local-Zone/ts/bin/config.ts
@@ -36,17 +36,17 @@ class AppConfig {
 
   /**
    * The version of Deadline to use on the render farm. Some examples of pinned version values are "10", "10.1", or
-   * "10.1.13"
-   * @default 10.1.13.2 is used, to match the worker AMI ID provided below
+   * "10.1.14"
+   * @default 10.1.14.5 is used, to match the worker AMI ID provided below
    */
-  public readonly deadlineVersion: string = '10.1.13.2';
+  public readonly deadlineVersion: string = '10.1.14.5';
 
   /**
-   * A map of regions to Deadline Client Linux AMIs. As an example, the Linux Deadline 10.1.13.2 AMI ID from us-west-2
+   * A map of regions to Deadline Client Linux AMIs. As an example, the Linux Deadline 10.1.14.5 AMI ID from us-west-2
    * is filled in. It can be used as-is, added to, or replaced. Ideally the version here should match the one in
    * package.json used for staging the render queue and usage based licensing recipes.
    */
-  public readonly deadlineClientLinuxAmiMap: Record<string, string> = {['us-west-2']: 'ami-0237f13ce87af168e'};
+  public readonly deadlineClientLinuxAmiMap: Record<string, string> = {['us-west-2']: 'ami-03b0cb310cfc17d53'};
 
   /**
    * (Optional) The name of the EC2 keypair to associate with instances.
