9b18afd44fce181ab11fe365fe9232ed79b19de6
Josh Usiskin <56369778+jusiskin@users.noreply.github.com>
2021-05-17 17:00:50 -0500

    chore(deps): bumping cdk from 1.102.0 to 1.104.0 (#429)
    * chore(deps): bumping cdk from 1.102.0 to 1.104.0
    
    * chore: tidy up package.json files
    
    Co-authored-by: Jericho Tolentino <jericht@amazon.com>

--- examples/deadline/All-In-AWS-Infrastructure-Basic/python/setup.py
+++ examples/deadline/All-In-AWS-Infrastructure-Basic/python/setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     packages=setuptools.find_packages(where="package"),
 
     install_requires=[
-        "aws-cdk.core==1.102.0",
+        "aws-cdk.core==1.104.0",
         "aws-rfdk==0.31.0"
     ],
 
