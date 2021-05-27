b86a416eca0517fd822e4f159a5d2eaeef9695eb
Jericho Tolentino <jericht@amazon.com>
2021-05-27 16:54:58 +0000

    chore(deps): bumping cdk from 1.104.0 to 1.106.1

--- examples/deadline/All-In-AWS-Infrastructure-SEP/python/setup.py
+++ examples/deadline/All-In-AWS-Infrastructure-SEP/python/setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     packages=setuptools.find_packages(where="package"),
 
     install_requires=[
-        "aws-cdk.core==1.104.0",
+        "aws-cdk.core==1.106.1",
         "aws-rfdk==0.32.0"
     ],
 
