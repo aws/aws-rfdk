ceafaad07a9a48423e6d7e647f834aea818923d0
RFDK Team <aws-rfdk@amazon.com>
2021-05-28 01:40:44 +0000

    chore(release): 0.33.0

--- examples/deadline/EC2-Image-Builder/python/setup.py
+++ examples/deadline/EC2-Image-Builder/python/setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
         "aws-cdk.aws-ec2==1.106.1",
         "aws-cdk.aws-s3-assets==1.106.1",
         "aws-cdk.core==1.106.1",
-        "aws-rfdk==0.32.0",
+        "aws-rfdk==0.33.0",
     ],
 
     python_requires=">=3.7",
