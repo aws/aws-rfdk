b917cc0aac757b2095b41a6184d0f44892141b8a
Josh Usiskin <usiskin@amazon.com>
2021-07-08 22:33:37 +0000

    chore(deps): bumping cdk from 1.108.1 to 1.111.0

--- examples/deadline/EC2-Image-Builder/python/setup.py
+++ examples/deadline/EC2-Image-Builder/python/setup.py
@@ -17,11 +17,11 @@ setuptools.setup(
     packages=setuptools.find_packages(where="package"),
 
     install_requires=[
-        "aws-cdk.aws-iam==1.108.1",
-        "aws-cdk.aws-imagebuilder==1.108.1",
-        "aws-cdk.aws-ec2==1.108.1",
-        "aws-cdk.aws-s3-assets==1.108.1",
-        "aws-cdk.core==1.108.1",
+        "aws-cdk.aws-iam==1.111.0",
+        "aws-cdk.aws-imagebuilder==1.111.0",
+        "aws-cdk.aws-ec2==1.111.0",
+        "aws-cdk.aws-s3-assets==1.111.0",
+        "aws-cdk.core==1.111.0",
         "aws-rfdk==0.35.0",
     ],
 
