7ea5f9e41f5501519deacec8c10cc11e89744f1f
David Horsman <horsmand@amazon.com>
2021-10-25 14:37:37 +0000

    fix(integ): increased cidr range for render queue alb subnet

--- integ/components/_infrastructure/lib/network-tier.ts
+++ integ/components/_infrastructure/lib/network-tier.ts
@@ -32,7 +32,17 @@ export class NetworkTier extends Stack {
     renderQueueAlb: {
       name: 'RenderQueueAlbSubnets',
       subnetType: SubnetType.PRIVATE,
-      cidrMask: 27,
+      // Current RenderQueueStructs:
+      //   deadline_02_renderQueue: 2
+      //   deadline_03_workerFleetHttp: 2
+      //   deadline_04_workerFleetHttps: 2
+      //   deadline_05_secretsManagement: 1
+      // 7 total
+      // If we choose a CIDR mask of 25, we get 2^(32-25)-2 = 126 IP addresses
+      // 126/7 = 18 IP addresses per RenderQueue
+      // Recommended addresses is 30 per subnet with a minimum of 8, so /25 gives us a little room before we hit the minimum. Refer to:
+      // https://docs.aws.amazon.com/elasticloadbalancing/latest/application/application-load-balancers.html#subnets-load-balancer
+      cidrMask: 25, // 2^(32-25)-2 = 126 IP addresses
     },
     sepFleet: {
       name: 'SepFleetSubnets',
