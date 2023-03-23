30b5facce9191eafd3820e6d9bd7de6f7b6553f6
Joel Wong <wonjoel@amazon.com>
2023-03-23 11:26:46 -0500

    chore(deps): bump aws-cdk from 2.54.0 to 2.70.0 (#1004)
    Bumps [aws-cdk](https://github.com/aws/aws-cdk/tree/HEAD/packages/aws-cdk) from 2.54.0 to 2.70.0.
    - [Release notes](https://github.com/aws/aws-cdk/releases)
    - [Changelog](https://github.com/aws/aws-cdk/blob/main/CHANGELOG.v2.md)
    - [Commits](https://github.com/aws/aws-cdk/commits/v2.70.0/packages/aws-cdk)
    
    ---
    updated-dependencies:
    - dependency-name: aws-cdk
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...

--- examples/deadline/All-In-AWS-Infrastructure-Basic/python/setup.py
+++ examples/deadline/All-In-AWS-Infrastructure-Basic/python/setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     packages=setuptools.find_packages(where="package"),
 
     install_requires=[
-        "aws-cdk-lib==2.54.0",
+        "aws-cdk-lib==2.70.0",
         "aws-rfdk==1.1.0"
     ],
 
